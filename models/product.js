d1ae2d6bd6b887cf70d2464782be084f38977e3a
SPrathamesh15 <sprathamesh354@gmail.com>
2024-03-06 14:09:27 +0530

    Added CRUD operation using MongoDb commands

--- models/product.js
+++ models/product.js
@@ -1,18 +1,29 @@
 const getDb = require('../util/database').getDb;
+const mongodb = require('mongodb');
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = new mongodb.ObjectId(id);
   }
 
   save() {
     const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
+    let dbOp;
+    if (this._id){
+      //update the product
+      dbOp = db
+        .collection('products')
+        .updateOne({ _id: this._id }, { $set: this })
+    } else{
+      dbOp = db
+        .collection('products')
+        .insertOne(this)
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
@@ -34,6 +45,27 @@ class Product {
       .catch(err => {
         console.log(err);
       });
+  }
+
+  static findById(prodId){
+    const db = getDb();
+    return db.collection('products')
+    .find({ _id: new mongodb.ObjectId(prodId) })
+    .next()
+    .then(product => {
+      console.log(product)
+      return product
+    })
+    .catch(err => console.log(err))
+  }
+
+  static deleteByID(prodId){
+    const db = getDb();
+    return db
+      .collection('products')
+      .deleteOne({ _id: new mongodb.ObjectId(prodId)})
+      .then(result => console.log('Deleted!'))
+      .catch(err => console.log(err))
   }
 }
 
